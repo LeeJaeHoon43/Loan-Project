c6ba4bc02b59f40b429c6f3d48ba13d8dc4d5f77
LeeJaeHoon43 <ljw160791@gmail.com>
2023-06-06 21:55:58 +0900

    #02 -  대출 상담 삭제 기능 구현

--- src/main/java/com/example/loan/service/CounselService.java
+++ src/main/java/com/example/loan/service/CounselService.java
@@ -6,4 +6,5 @@ public interface CounselService {
     CounselDTO.Response create(CounselDTO.Request request);
     CounselDTO.Response get(Long counselId);
     CounselDTO.Response update(Long counselId, CounselDTO.Request request);
+    void delete(Long counselId);
 }
